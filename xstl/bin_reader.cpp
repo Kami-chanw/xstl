e14e10109861b5e1c4db9ba829e1fdce42c03808
kamichanw <865710157@qq.com>
2022-11-16 19:03:56 +0800

    new ver

--- xstl/bin_reader.cpp
+++ xstl/bin_reader.cpp
@@ -1,42 +1,42 @@
+#define _LEVEL_ORDER_ITERATOR_
 #include "bs_tree.hpp"
 #include <algorithm>
+#include <format>
 #include <iostream>
 #include <list>
 #include <ranges>
+#include <set>
+#include <xtree>
 using namespace std;
 namespace rng = ranges;
-//clang-format off
-template <class _Se, class _It>
-concept s_for = copyable<_Se> && default_initializable<_Se> && input_or_output_iterator<_It> && _Weakly_equality_comparable_with<_Se, _It>;
-template <class _Ty>
-concept _Has_member = requires(_Ty __t) {
-    { __t.end() } -> s_for<rng::iterator_t<_Ty>>;
-};
-template <_Has_member Rng>
-void f(Rng) {}
-int  main() {
-    list s{ 1, 2, 3 };
-    auto t = s.begin();
-    s.pop_front();
-    *t;
+
+int main() {
+    xstl::bs_set<int> b{ 5, 3, 6, 2, 4, 8, 7 };
+    b.display(cout);
+
+    for (auto i : b)
+        cout << i << " ";
+    cout << endl;
+    for (auto i : views::reverse(b))
+        cout << i << " ";
 }
-//#include <xstring>
-//#include "bs_tree.hpp"
-//#include "bitstring.hpp"
-//#include "utility.hpp"
-//#include "huffman.hpp"
-// using namespace xstl;
-// using namespace std;
-// template <class T>
-// void print(const T& val) {
+// #include <xstring>
+// #include "bs_tree.hpp"
+// #include "bitstring.hpp"
+// #include "utility.hpp"
+// #include "huffman.hpp"
+//  using namespace xstl;
+//  using namespace std;
+//  template <class T>
+//  void print(const T& val) {
 //	for (int i = 0; i < TYPE_BIT(T); ++i)
 //		cout << (get_bits(val, i, i + 1) != 0);
 //	cout << endl;
-//}
+// }
 //
-// int main() {
-//    bs_set<int> b{ 1, 2, 3 }, b2;
-//    b.swap(b2);
+//  int main() {
+//     bs_set<int> b{ 1, 2, 3 }, b2;
+//     b.swap(b2);
 //	{
 //		/*LARGE_INTEGER t1, t2, tc, tr{};
 //		for (int i = 0; i < 100000; ++i) {
@@ -48,7 +48,7 @@ int  main() {
 //
 //		}
 //		cout << tr.QuadPart;*/
-//        /* cout << "1001"_bstr[ 0, 2_ri ];
+//         /* cout << "1001"_bstr[ 0, 2_ri ];
 //
 //		basic_bitstring<unsigned char> bs("1001011"), bs2("10001");
 //		//bs[0, 6_ri].assign(bs[0,3_ri]);
@@ -67,4 +67,4 @@ int  main() {
 //
 //	}
 //	_CrtDumpMemoryLeaks();
-//}
+// }
